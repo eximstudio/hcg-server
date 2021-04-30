6fe2959fbca3e4b31f6a42e65729d94285c3274b
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-04-30 12:49:06 +0530

    init

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const json = require('./data.json')
+
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.post('/test', (req, res) => {
+	res.status(201)
+	res.set("Access-Control-Allow-Origin", req.query.name)
+  res.json(json)
+	console.log(req.query.name)
+});
+
+app.get('/', (req, res) => {
+	res.redirect('https://eximstudio.com')
+})
+
+app.listen(3000, () => {
+  console.log('server started');
+});
