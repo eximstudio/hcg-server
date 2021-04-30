1485562c4351c5ff4e8f4ce109dceab3fdeb1902
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-04-30 19:42:22 +0530

    minor

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 const express = require('express');
 const json = require('./data.json')
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
 
 const app = express();
 
@@ -27,6 +21,6 @@ app.get('/', (req, res) => {
 	res.redirect('https://eximstudio.com')
 })
 
-app.listen(3000, () => {
+app.listen(4000, () => {
   console.log('server started');
 });
