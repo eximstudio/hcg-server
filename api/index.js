19468840dcc9dcefe12ca671bf910d4403fc5bee
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-07-08 10:07:19 +0530

    test001

--- api/index.js
+++ api/index.js
@@ -1,26 +1,7 @@
-const express = require('express');
-const json = require('./data.json')
+const json = require("./data.json")
 
-const app = express();
-
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-
-app.post('/test', (req, res) => {
+module.exports = (req, res) => {
 	res.status(201)
 	res.set("Access-Control-Allow-Origin", req.query.origin)
 	res.json(json)
-});
-
-app.post('/restart/'+process.env.TOKEN, (req, res) => {
-		res.sendStatus(200)
-		process.exit(2)
-})
-
-app.get('/', (req, res) => {
-	res.redirect('https://eximstudio.com')
-})
-
-app.listen(4000, () => {
-  console.log('server started');
-});
+}
--- main.sh
+++
@@ -1,6 +0,0 @@
-while [ true ]; do
-clear
-git fetch --all
-git reset --hard origin/main
-node api/index.js
-done
