54f870c2150a9e1a05327a91e5ab468238495dcf
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-07-08 11:11:09 +0530

    test010

--- api/index.js
+++ api/index.js
@@ -82,8 +82,8 @@ let json = [
 
 module.exports = (req, res) => {
   res.status(201);
-  // res.headers.set("Access-Control-Allow-Origin", "*");
-  console.log(Object.entries(res));
-  console.log(res.setHeader);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // console.log(Object.entries(res));
+  // console.log(res.setHeader);
   res.json(json);
 };
